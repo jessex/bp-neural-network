68e7e974fb2fa0fc9b6275b6d7ae98b5e35629a7
Joshua Essex <essexjosh@gmail.com>
2010-11-18 12:28:46 -0500

    Simple main file for testing purposes

---
+++ test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "backprop.h"
+
+int main(void) {
+    init_rand();
+    neural_network net; 
+    neural_network *net_ptr = &net;
+    initialize_network(net_ptr,2,2,1);
+    
+    train_network(net_ptr,4,1000,0.5,0.1);
+    test_network(net_ptr,4);
+    
+    
+    return 0;
+}
+
+
